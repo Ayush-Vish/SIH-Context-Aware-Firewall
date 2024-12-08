26283510064f807fc2e61edae67d262453b93d62
Ayush Vishwakarma <ayushvish6555@gmail.com>
2024-12-08 16:44:51 +0530

    refactor

--- SERVERS/Node_server/db/client.js
+++ SERVERS/Node_server/db/client.js
@@ -1,77 +1,84 @@
 import mongoose from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
-
 const clientSchema = new mongoose.Schema({
     clientID: { 
         type: String,  
         unique: true, 
-      },
+    },
     adminID: { 
         type: String,
     },
     mac_addresses: [String],
     created_at: { type: Date, default: Date.now },
     last_seen: { type: Date },
+    active_list: {
+        type: String,
+        enum: ['whitelist', 'blacklist'],
+        default: null
+    },
+    active_rules: [{
+        appName: String,
+        rules: [String]
+    }]
 });
+const ruleSchema = new mongoose.Schema({
+  clientID: {
+      type: String,
+      required: true,
+      index: true
+  },
+  type: {
+      type: String,
+      enum: ['application', 'domain', 'port'],
+      required: true
+  },
+  rule_name: {
+      type: String,
+      required: true
+  },
+  domain: {
+      type: String,
+      required: function() { return this.type === 'domain'; }
+  },
+  app_path: {
+      type: String,
+      required: function() { return this.type === 'application'; }
+  },
+  direction: {
+      type: String,
+      enum: ['inbound', 'outbound'],
+      required: true
+  },
+  ports: [{
+      type: Number,
+      min: 1,
+      max: 65535
+  }],
+  action: {
+      type: String,
+      enum: ['allow', 'block'],
+      default: 'block'
+  },
+  status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'active'
+  },
+  created_by: {
+      type: String,
+      required: true
+  },
+  ip_addresses: [{
+      type: String
+  }],
+  last_ip_update: {
+      type: Date,
+      default: Date.now
+  }
+});
+export const Rule = mongoose.model("Rule", ruleSchema);
 
-const clientRules = new mongoose.Schema({
-    clientID: {
-        type: String,
-        required: true,
-        index: true
-      },
-      type: {
-        type: String,
-        enum: ['application', 'domain', 'port'],
-        required: true
-      },
-      rule_name: {
-        type: String,
-        required: true
-      },
-      domain: {
-        type: String,
-        required: true
-      },
-      app_path: {
-        type: String,
-        required: function() { return this.type === 'application'; }
-      },
-      direction: {
-        type: String,
-        enum: ['inbound', 'outbound'],
-        required: true
-      },
-      ports: [{
-        type: Number,
-        min: 1,
-        max: 65535
-      }],
-      action: {
-        type: String,
-        enum: ['allow', 'block'],
-        default: 'block'
-      },
-      status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'active'
-      },
-      created_by: {
-        type: String,
-        required: true
-      },
-      ip_addresses: [{
-        type: String
-      }],
-      last_ip_update: {
-        type: Date,
-        default: Date.now
-      }
-})
-
-clientRules.index
 export const findClientByMAC = async (macAddresses) => {
     try {
         // Search for the client by checking if any of the MAC addresses match the stored ones
@@ -110,7 +117,5 @@ export const createClientByMAC = async (macAddresses) => {
         throw err;
     }
 };
-
-
 
 export const Client = mongoose.model("Client", clientSchema);
